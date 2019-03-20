6cfc824f877d8a0f56975968232840c37cb12b06
Max Amsterdam <maxwell.amsterdam@columbia.edu>
2019-03-19 20:21:55 -0400

    Added several functionalities in this commit.
    Reorganized structure of application to have a main flask file (app.py) and a helper file (helpers.py).
    Added mysql database integration.
    Added ability to store access token in database, and have the application automatically refresh the access token when neccessary.
    Added ability to give a date range and display a ranked list of all athletes based on their respective number of hours within that range.

--- app.py
+++ app.py
@@ -1,165 +1,87 @@
-from authlib.client import OAuth2Session, OAuthClient
+from authlib.client import OAuth2Session
 
-from flask import Flask, request, render_template, redirect, session, url_for
-from flask.json import jsonify
-import os, requests, datetime, MySQLdb
+from flask import Flask, request, render_template, redirect
+from operator import itemgetter
+import os
+import requests
+import helpers
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
 client_id = 'columbiac150'
 client_secret = 'kzSN7CYgZYUMzb8DfhEqRnqrHAqiAEUHOgSAJo8'
-scope = ["coach:athletes", "workouts:read"]
+coach_scope = ["coach:athletes", "workouts:read"]
 redirect_uri = "https://localhost:5000"
 authorization_base_url = 'https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize'
-token_url = 'https://oauth.sandbox.trainingpeaks.com/oauth/token'
-api_base_url = 'https://api.sandbox.trainingpeaks.com'
-db_host = "mysql.c150data.com"
-db_name = "c150db"
-db_user = "lwtpoodles150"
-db_passwd = "Columbia150"
+token_base_url = 'https://oauth.sandbox.trainingpeaks.com/oauth/token'
 
 
-def connectToDB():
-    return MySQLdb.connect(host=db_host, user=db_user, passwd=db_passwd,
-            db=db_name)
-
-
-@app.route("/db/insert")
-def executeTokenInsert(access_token, token_type, expires_in, refresh_token,
-        scope):
-    expires_at_date = datetime.datetime.now() + datetime.timedelta(hours=1)
-    expires_at_date = expires_at_date.replace(microsecond=0)
-    statement = "INSERT INTO `access_token` (`access_token`, `token_type`,`expires_at`, `refresh_token`, `scope`) VALUES ('{}', '{}', '{}', '{}','{}');".format(
-            access_token, token_type, expires_at_date, refresh_token, scope)
-    print(statement)
-
-    conn = connectToDB()
-    cursor = conn.cursor()
-    try:
-        cursor.execute(statement)
-        conn.commit()
-        print("Inserted access token with statement %s", statement)
-    except MySQLdb.IntegrityError:
-        print("Failed to insert values")
-    finally:
-        conn.close()
-
-
-def getToken():
-    ensureToken()
-    conn = connectToDB()
-    cursor = conn.cursor()
-
-    try:
-        cursor.execute("SELECT access_token FROM access_token")
-        access_token = cursor.fetchone()[0]
-    except MySQLdb.IntegrityError:
-        print("Failed to fetch access_token")
-    finally:
-        conn.close()
-
-    return access_token
-
-
-
-
-def getHeaders():
-    token = getToken()
-    return {'host': 'api.sandbox.trainingpeaks.com', 'content-type':
-            'application/json', 'Authorization': 'Bearer ' + token}
-
-
-def ensureToken():
-    token_needs_refresh = False # TODO: Refresh token stuff
-    if token_needs_refresh:
-        oauth_session = OAuth2Session(client_id, client_secret,
-                redirect_uri=redirect_uri, scope=scope)
-        authorization_response = input(str("authorization_response: "))
-        token = oauth_session.fetch_access_token(token_url,
-                authorization_response=authorization_response)
-        executeTokenInsert(access_token=token['access_token'],token_type=token['token_type'],expires_in=token['expires_in'],refresh_token=token['refresh_token'],scope=token['scope'])
-
-
-def getAthleteId():
-    r = requests.get('https://api.sandbox.trainingpeaks.com/v1/athlete/profile',
-            headers=getHeaders())
-    print(r)
-    id = (r.json())["Id"]
-    print(id)
-    return id
-
-
-def getHoursForAthlete(id, start_date, end_date):
-    api_url = api_base_url + '/v1/workouts/{}/{}/{}'.format(id, start_date, end_date)
-
-    response = requests.get(api_url, headers=getHeaders())
-
-    json_response = response.json()
-    sum_hours = 0
-    for workout in json_response:
-        total_time = workout['TotalTime']
-        print("time: ", total_time)
-        if type(total_time) is float:
-            sum_hours += float(total_time)
-    return sum_hours
-
-
-@app.route("/athletes")
-def getAllAthletes():
-    ensureToken()
-    r = requests.get('https://api.sandbox.trainingpeaks.com/v1/coach/athletes',
-                      headers=getHeaders())
-    athletes = list()
-    r_json = r.json()
-    for athlete in r_json:
-        hours = getHoursForAthlete(athlete['Id'], '2019-02-01', '2019-03-10')    
-        athletes.append("{} {}: {}".format(athlete['FirstName'],
-                                           athlete['LastName'], hours))
-
-    print(athletes)
-    return render_template("index.html", len=len(athletes), athletes=athletes)
-
-
-
-@app.route("/hours")
-def getHoursThisWeek():
-    ensureToken()
-    id = getAthleteId()
-
-    start_date = '2019-03-03'
-    end_date = '2019-03-10'
-
-    api_url = api_base_url + '/v1/workouts/{}/{}/{}'.format(id, start_date, end_date)
-    response = requests.get(api_url, headers=getHeaders())
-
-    json_response = response.json()
-    sum_hours = 0
-    count = 1
-    for workout in json_response:
-        total_time = workout['TotalTime']
-        print("time: ", total_time)
-        if type(total_time) is float:
-            sum_hours += float(total_time)
-        count += 1
-
-    print("count", count)
-
-    return render_template("index.html", last_weeks_hours=sum_hours)
-
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        print("Start date: ", start_date)
+        print("End date: ", end_date)
+        len, athletes = getAllAthletes(start_date, end_date)
+        return render_template("index.html", len=len, athletes=athletes)
+    else:
+        return render_template("index.html")
 
 
 @app.route("/authorize")
 def user_authorization():
     oauth_session = OAuth2Session(client_id, client_secret, redirect_uri=redirect_uri,
-            scope=scope)
+                                  scope=coach_scope)
     authorization_url, state = oauth_session.create_authorization_url(authorization_base_url)
     print(authorization_url)
     return redirect(authorization_url)
+
+
+@app.route("/insertNewToken")
+def insertNewToken():
+    oauth_session = OAuth2Session(client_id, client_secret,
+                                  redirect_uri=redirect_uri, scope=coach_scope)
+    authorization_response = input(str("authorization_response: "))
+    token = oauth_session.fetch_access_token(token_base_url,
+                                             authorization_response=authorization_response)
+    helpers.executeTokenInsert(token)
+    return render_template("index.html")
+
+
+@app.route("/getData")
+def getData():
+    print("started")
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    len, athletes = getAllAthletes(start_date, end_date)
+    return render_template("data.html", len=len, athletes=athletes)
+
+
+def getAllAthletes(start_date, end_date):
+    headers = helpers.getHeaders()
+    r = requests.get('https://api.sandbox.trainingpeaks.com/v1/coach/athletes',
+                     headers=headers)
+    athletes = list()
+    r_json = r.json()
+    for athlete in r_json:
+        # date formatted YYYY-MM-DD
+        hours = helpers.getHoursForAthlete(athlete['Id'],
+                                           start_date, end_date)
+        rounded_hours = round(hours, 2)
+        athlete_info = {
+            "name": "{} {}".format(athlete["FirstName"], athlete["LastName"]),
+            "hours": hours,
+            "rounded_hours": rounded_hours
+        }
+        athletes.append(athlete_info)
+
+    # sort list of athletes based on number of hours
+    sorted_athletes = sorted(athletes, key=itemgetter('hours'), reverse=True)
+    print(sorted_athletes)
+
+    return (len(athletes), sorted_athletes)
 
 
 if __name__ == "__main__":
---
+++ helpers.py
@@ -0,0 +1,158 @@
+"""
+Helper methods for the c150.data data retrieval process
+"""
+
+import MySQLdb
+import datetime
+import requests
+from authlib.client import OAuth2Session
+
+api_base_url = 'https://api.sandbox.trainingpeaks.com'
+
+db_host = "mysql.c150data.com"
+db_name = "c150db"
+db_user = "lwtpoodles150"
+db_passwd = "Columbia150"
+
+grant_type = "refresh_token"
+client_id = 'columbiac150'
+client_secret = 'kzSN7CYgZYUMzb8DfhEqRnqrHAqiAEUHOgSAJo8'
+refresh_url = "https://oauth.sandbox.trainingpeaks.com/oauth/token"
+redirect_uri = "https://localhost:5000"
+
+
+def connectToDB():
+    return MySQLdb.connect(host=db_host, user=db_user, passwd=db_passwd,
+                           db=db_name)
+
+def deleteToken(conn):
+    if conn is None:
+        conn = connectToDB()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * from access_token")
+    if cursor.rowcount == 1:
+        try:
+            cursor.execute("DELETE FROM access_token")
+            conn.commit()
+            print("Deleted expired token")
+        except MySQLdb.IntegrityError:
+            print("Failed to delete access token row.")
+        finally:
+            conn.close()
+            return 1
+
+    return None 
+
+
+def executeTokenInsert(token):
+    access_token = token['access_token']
+    token_type = token['token_type']
+    expires_in = token['expires_in']
+    refresh_token = token['refresh_token']
+    scope = token['scope']
+    # Expires in 1 hour, I give it an extra 5 minutes as padding time
+    expires_at_date = datetime.datetime.now() + datetime.timedelta(seconds=(int(expires_in)-300))
+    expires_at_date = expires_at_date.replace(microsecond=0)
+    insert_statement = "INSERT INTO `access_token` (`access_token`, `token_type`,`expires_at`, `refresh_token`, `scope`) VALUES ('{}', '{}', '{}', '{}','{}');".format(
+            access_token, token_type, expires_at_date, refresh_token, scope)
+    select_statement = "SELECT * FROM 'access_token'"
+    conn = connectToDB()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(select_statement)
+        if cursor.rowcount > 0:
+            deleteToken(conn)
+        cursor.execute(insert_statement)
+        conn.commit()
+        print("Inserted access token with statement", insert_statement)
+    except MySQLdb.IntegrityError:
+        print("Failed to insert values")
+    finally:
+        conn.close()
+
+
+def refreshTokenIfNeeded():
+    conn = connectToDB()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM access_token")
+    if cursor.rowcount != 1:
+        print("Error: more than one token in access_token table") 
+        return None 
+
+    row = cursor.fetchone()
+    refresh_date = row[2]
+
+    needsRefreshing = True if refresh_date < datetime.datetime.now() else False
+
+    # update if necessary
+    if needsRefreshing:
+        print("Refreshing token...")
+        refresh_token = row[3]
+        oauth_session = OAuth2Session(client_id, client_secret, refresh_token=refresh_token)
+        body = "grant_type=refresh_token"
+        updated_token = oauth_session.refresh_token(refresh_url,
+                                                    refresh_token=refresh_token,
+                                                    body=body)
+        deleteToken()
+        executeTokenInsert(updated_token)
+        return updated_token
+    else:
+        # token does not need to be updated
+        return 1 
+
+
+def getToken():
+    # Retruns None on error
+    if refreshTokenIfNeeded() is None:
+        print("Error: Refresh Token returned None")
+        return None
+
+    conn = connectToDB()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM access_token")
+        num_rows = cursor.rowcount
+        if num_rows == 0:
+            print("Error: An access token does not exist")
+        elif num_rows == 1:
+            token = cursor.fetchone()
+            return token
+        else:
+            print("There are multiple access keys in the database. This is not allowed")
+    except MySQLdb.IntegrityError:
+        print("Failed to fetch access_token")
+    finally:
+        conn.close()
+
+
+def getHeaders():
+    access_token = getToken()[0]
+    return {'host': 'api.sandbox.trainingpeaks.com','content-type':
+            'application/json', 'Authorization': 'Bearer ' + access_token}
+
+
+
+
+def getAthleteId():
+    r = requests.get('https://api.sandbox.trainingpeaks.com/v1/athlete/profile',
+                     headers=getHeaders())
+    id = (r.json())["Id"]
+    return id
+
+
+def getHoursForAthlete(id, start_date, end_date):
+    print("Getting hours for athlete {}...".format(id))
+    api_url = api_base_url + '/v1/workouts/{}/{}/{}'.format(id, start_date, end_date)
+    response = requests.get(api_url, headers=getHeaders())
+
+    json_response = response.json()
+    sum_hours = 0
+    for workout in json_response:
+        total_time = workout['TotalTime']
+        if type(total_time) is float:
+            sum_hours += float(total_time)
+    return sum_hours
