6e16e6f7c2d48e165392066f95fe4d4e63f8f343
Lukas Geiger <Lwt>
2019-07-09 13:41:10 -0400

    Adding small remote level changes

--- app/__init__.py
+++ app/__init__.py
@@ -39,8 +39,8 @@ from app import admin
 
 from app import routes  # position important to avoid circular importation
 from app.models import User, AuthToken, Athlete, Workout
-#Workout.__table__.drop(db.engine) # Use if you want to drop the table and reset it
-#Athlete.__table__.drop(db.engine) # Use if you want to drop the table and reset it
+Workout.__table__.drop(db.engine) # Use if you want to drop the table and reset it
+Athlete.__table__.drop(db.engine) # Use if you want to drop the table and reset it
 db.create_all()  # Only creates tables when they do not already exist
 db.session.commit()
 
