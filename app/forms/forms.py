4df54c24a4af22b29dd5787be4465ff8f86d2a71
Lukas Geiger <geiger.ljo@gmail.com>
2019-07-22 15:53:37 +0200

    Created Forgot Password workflow. Changed files with 'hours' to 'data'. Edited README

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -56,3 +56,27 @@ class ContactForm(FlaskForm):
     message = StringField('Feedback, Comments, Concerns?', widget=TextArea(),
                           validators=[DataRequired()])
     submit = SubmitField('Send')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):  # check if email exists
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError(
+                'There is no account with that email. Please contact an administrator to create an account.')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,8 @@ from app.database import db_filler
 from app.admin import admin
 from flask_mail import Message
 from app.db_models import User
-from app.forms.forms import RegistrationForm, LoginForm, ContactForm
+from app.forms.forms import (RegistrationForm, LoginForm, ContactForm, 
+                            RequestResetForm, ResetPasswordForm)
 
 # Admin decorator
 from functools import wraps
@@ -57,10 +58,10 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/hours", methods=['GET', 'POST'])
+@app.route("/data", methods=['GET', 'POST'])
 @requires_access_level(ACCESS['user'])
-def hours():
-    return render_template("hours.html")
+def data():
+    return render_template("data.html")
 
 
 @app.route("/contact", methods=['GET', 'POST'])
@@ -87,7 +88,7 @@ def contact():
 # DATA
 
 
-@app.route("/hours/getData")
+@app.route("/data/getData")
 @requires_access_level(ACCESS['user'])
 def getData():
     """
@@ -148,6 +149,49 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', 
+        sender='lwtpoodles150@gmail.com', 
+        recipients=[user.email])
+    # Format of this is important or tabs will be shown in the email
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made. 
+'''
+    mail.send(msg)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('about'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.','info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('about'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token','warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():  # Enters this section when registration form has been submitted
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated. You are now able to log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
 
 
 # ADMIN
