3df5cb657ebd03167f94c3d7488c16961e07141d
Max <mamsterdam@cardconnect.com>
2019-07-22 13:48:09 -0400

    Quick bug fix for total hours

--- app/static/js/hours.js
+++ app/static/js/hours.js
@@ -112,6 +112,7 @@ $(function() {
            dataType: 'json',
            data: getData(),
            success: function(response) {
+               total = response['total_hours']
                params.success(response['athlete_list'], response['total_hours']);
            },
            error: function(e) {
