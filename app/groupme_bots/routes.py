2cad3ee5c780859f67e25ae72d12b2405aa5fd15
Max Amsterdam <maxwell.amsterdam@columbia.edu>
2019-11-07 13:30:01 -0500

    Minor addition to groupme

--- app/groupme_bots/routes.py
+++ app/groupme_bots/routes.py
@@ -7,9 +7,7 @@ groupme = Blueprint('groupme', __name__)
 
 @groupme.route("/groupme/testbot", methods=['POST'])
 def testbot():
-    log.info('Groupme received the following args: {}'.format(request.args))
-    log.info('Sending response...')
-    r = requests.post('https://api.groupme.com/v3/bots/post', params={"text" : "Hello there!", "bot_id" : "9d8e84f1d16b3275ee6da38976"})
-    log.info('got repsonse: {}'.format(r))
+    log.info('Groupme received the following args: {}'.format(request.args.items))
+    # r = requests.post('https://api.groupme.com/v3/bots/post', params={"text" : "Hello there!", "bot_id" : "9d8e84f1d16b3275ee6da38976"})
     return Response('Received and sent a message!')
     
