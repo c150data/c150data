001ef986772d869fa9a9e974bb392764ec576c06
Max Amsterdam <maxwell.amsterdam@columbia.edu>
2019-11-07 13:46:47 -0500

    Further changes to groupme bot

--- app/groupme_bots/routes.py
+++ app/groupme_bots/routes.py
@@ -1,6 +1,7 @@
 
 from flask import Blueprint, request, Response, render_template, redirect, flash
 from app import log
+import random
 import requests
 
 groupme = Blueprint('groupme', __name__)
@@ -8,6 +9,30 @@ groupme = Blueprint('groupme', __name__)
 @groupme.route("/groupme/testbot", methods=['POST'])
 def testbot():
     log.info('Groupme received the following args: {}'.format(request.args.items))
-    # r = requests.post('https://api.groupme.com/v3/bots/post', params={"text" : "Hello there!", "bot_id" : "9d8e84f1d16b3275ee6da38976"})
+    log.info('Groupme received the following args: {}'.format(request.args.items))
+    name = request.args['name'].lower()
+    test = request.args['text']
+    possibilities = [
+        'shut the fuck up Ishan',
+        'sHuT ThE fUCk uP iSHaN',
+        'Shut the FUCK up Ishan'
+    ]
+    selection = random.randint(0,2)
+    if name == 'max amsterdam':
+        r = requests.post('https://api.groupme.com/v3/bots/post', params={"text" : possibilities[selection], "bot_id" : "366a7bac14fc9609ff18849b6b"})
     return Response('Received and sent a message!')
     
+@groupme.route("/groupme/ishanbot", methods=['POST'])
+def testbot():
+    log.info('Groupme received the following args: {}'.format(request.args.items))
+    name = request.args['name'].lower()
+    test = request.args['text']
+    possibilities = [
+        'shut the fuck up Ishan',
+        'sHuT ThE fUCk uP iSHaN',
+        'Shut the FUCK up Ishan'
+    ]
+    selection = random.randint(0,2)
+    if name == 'ishan django':
+        r = requests.post('https://api.groupme.com/v3/bots/post', params={"text" : possibilities[selection], "bot_id" : "8ac02d8d84c2cae7301c0f5a5d"})
+    return Response('Received and sent a message!')
